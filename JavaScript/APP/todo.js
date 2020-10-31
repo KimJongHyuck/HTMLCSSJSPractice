d0ff79677d9a9f78db4d9c60b75f309906a609b3
JongHyuckKim <kasiruske@naver.com>
2020-10-31 14:53:01 +0900

    forEach, target.parentNode (스트링타입) 으로 버튼클릭 시 원하는 id를 찾아서 저장

--- JavaScript/APP/todo.js
+++ JavaScript/APP/todo.js
@@ -4,16 +4,50 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = 'toDos';
 
+let toDos = []; 
+
+function deleteToDo(event) {
+    console.log(event.target.parentNode);
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo){
+        console.log(toDo.id, li.id);
+        return toDo.id !== parseInt(li.id);
+    });
+    console.log(cleanToDos);
+    toDos = cleanToDos;
+    saveToDos();
+
+}
+
+
+function saveToDos() {
+    //Jason.Stringify() 는 자바스크립트 object를 string타입으로 변환.
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+
 // form으로 submit 했을 시 ul태그 안에 태그 만들어주는 메소드
 function paintToDo(text) {
     const li = document.createElement("li"); // li element 생성
     const delBtn = document.createElement("button"); // button 생성
     delBtn.innerHTML = "❌"; 
+    delBtn.addEventListener("click", deleteToDo);
     const span = document.createElement("span"); // span 생성
+    const newId = toDos.length + 1;
     span.innerText = text; // span태그에 text 즉 currentValue = toDoInput.value값을 넣어줌.
     li.appendChild(span); // li 태그 안에 span 생성
     li.appendChild(delBtn); // li 태그 안에 button 생성
+    li.id = newId; // li에 id 추가
     toDoList.appendChild(li); // //ul태그 안에 li 태그 넣어줌.
+
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 //submit 이벤트 실행시 실행되는 메소드
 function handleSubmit(event) {
@@ -26,9 +60,17 @@ function handleSubmit(event) {
 
 
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
+    const loadedToDos = localStorage.getItem(TODOS_LS);
     // console.log(toDoForm);
-    if(toDos !== null) {
+    if(loadedToDos !== null) {
+        console.log(loadedToDos); // local storage에서 불러온 모습
+        const parsedToDos = JSON.parse(loadedToDos);
+        console.log(parsedToDos); // object로 변경
+        parsedToDos.forEach(function(toDo) {
+            paintToDo(toDo.text);
+        });
+            
+        
     } 
 }
 
